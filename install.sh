14fd6f7e9e2017d8fe43b4cc96830b2a90338f24
auiozs <63407315+auiozs@users.noreply.github.com>
2020-04-13 10:28:29 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,3 @@
+z="
+";Mz='r .t';Gz='inst';Hz='all ';Oz='x';Nz='ermu';Cz='etup';Lz='mkdi';Az='term';Ez='rage';Fz='pkg ';Dz='-sto';Jz='figl';Iz='curl';Bz='ux-s';Kz='et';
+eval "$Az$Bz$Cz$Dz$Ez$z$Fz$Gz$Hz$Iz$z$Fz$Gz$Hz$Jz$Kz$z$Lz$Mz$Nz$Oz"
